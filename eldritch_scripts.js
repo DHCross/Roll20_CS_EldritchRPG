02c9f2635d4f82a430c1bd3e8762fd77bdaf408a
DHCross <45954119+DHCross@users.noreply.github.com>
2023-06-13 01:35:38 -0500

    change

--- eldritch_scripts.js
+++ eldritch_scripts.js
@@ -1,34 +1,19 @@
-on('change:prowess change:agility change:melee', () => {
-    getAttrs(['prowess', 'agility', 'melee'], values => {
-      const prowess = +values.prowess || 0;
-      const agility = +values.agility || 0;
-      const melee = +values.melee || 0;
-      const activeDefenseHP = prowess + agility + melee;
-      setAttrs({
-        active_defense_hp: activeDefenseHP
-      });
-    });
-  });
-  
-  on('change:fortitude change:endurance change:strength', () => {
-    getAttrs(['fortitude', 'endurance', 'strength'], values => {
-      const fortitude = +values.fortitude || 0;
-      const endurance = +values.endurance || 0;
-      const strength = +values.strength || 0;
-      const passiveDefenseHP = fortitude + endurance + strength;
-      setAttrs({
-        passive_defense_hp: passiveDefenseHP
-      });
-    });
-  });
-  
-  on('change:competence change:willpower', () => {
-    getAttrs(['competence', 'willpower'], values => {
-      const competence = +values.competence || 0;
-      const willpower = +values.willpower || 0;
-      const spiritPoints = competence + willpower;
-      setAttrs({
-        spirit_points: spiritPoints
-      });
-    });
+on("change:prowess change:agility change:melee change:fortitude change:endurance change:strength change:competence change:willpower", function() {
+    var prowess = parseInt(getAttrByName(characterId, "prowess"));
+    var agility = parseInt(getAttrByName(characterId, "agility"));
+    var melee = parseInt(getAttrByName(characterId, "melee"));
+    var fortitude = parseInt(getAttrByName(characterId, "fortitude"));
+    var endurance = parseInt(getAttrByName(characterId, "endurance"));
+    var strength = parseInt(getAttrByName(characterId, "strength"));
+    var competence = parseInt(getAttrByName(characterId, "competence"));
+    var willpower = parseInt(getAttrByName(characterId, "willpower"));
+    var activeDefense = prowess + agility + melee;
+    var passiveDefense = fortitude + endurance + strength;
+    var spiritPoints = competence + willpower;
+    setAttrByName(characterId, "active_defense_hp", activeDefense);
+    setAttrByName(characterId, "passive_defense_hp", passiveDefense);
+    setAttrByName(characterId, "spirit_points", spiritPoints);
+    setAttrByName(characterId, "calculated_active_defense_hp", activeDefense);
+    setAttrByName(characterId, "calculated_passive_defense_hp", passiveDefense);
+    setAttrByName(characterId, "calculated_spirit_points", spiritPoints);
   });
